2c11f36c9906611d23febcfe421c38c753ee30f5
Yoyo San <veselu@gmail.com>
2020-08-10 20:03:24 +0300

    fix: Left drawer not closing on page load

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,7 +15,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
+    <q-drawer v-model="leftDrawerOpen" content-class="bg-grey-1">
       <q-list>
         <q-item-label header class="text-grey-8">Lesson Links</q-item-label>
         <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
