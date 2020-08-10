4cafe186b05cb5ca10ab124c56dc3a65430e606e
Yoyo San <veselu@gmail.com>
2020-08-10 20:08:24 +0300

    config: Adjust quasar.conf.js

--- quasar.conf.js
+++ quasar.conf.js
@@ -67,12 +67,12 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // rtl: false, // https://quasar.dev/options/rtl-support
-      // preloadChunks: true,
+      preloadChunks: true,
       // showProgress: false,
-      // gzip: true,
+      gzip: true,
       // analyze: true,
 
       // Options below are automatically set depending on the env, set them if you want to override
