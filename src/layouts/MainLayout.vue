e0b3df11674d5167a2b22a43115621adab1f32c9
Yoyo San <veselu@gmail.com>
2020-08-10 20:00:23 +0300

    feat: Add day 1 stuff

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,31 +11,14 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title>
-          Flexbox and Grid for Responsive Web
-        </q-toolbar-title>
-
+        <q-toolbar-title>Flexbox and Grid for Responsive Web</q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Lesson Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item-label header class="text-grey-8">Lesson Links</q-item-label>
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -58,50 +41,7 @@ export default {
   data () {
     return {
       leftDrawerOpen: false,
-      essentialLinks: [
-        {
-          title: 'Lesson 1',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        },
-        {
-          title: 'Quasar Awesome',
-          caption: 'Community Quasar projects',
-          icon: 'favorite',
-          link: 'https://awesome.quasar.dev'
-        }
-      ]
+      essentialLinks: []
     }
   }
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,17 +5,16 @@
       src="https://drive.google.com/viewerng/
 viewer?embedded=true&url=https://raw.githubusercontent.com/jen4web/fem-layout/master/day1.pdf"
       width="90%"
-      height="500px"
-    >
+      height="650px"
+    />
 
     <div class="q-my-lg text-h5">Day 2</div>
     <embed
       src="https://drive.google.com/viewerng/
 viewer?embedded=true&url=https://raw.githubusercontent.com/jen4web/fem-layout/master/day2.pdf"
       width="90%"
-      height="500px"
-    >
-
+      height="650px"
+    />
   </q-page>
 </template>
 
