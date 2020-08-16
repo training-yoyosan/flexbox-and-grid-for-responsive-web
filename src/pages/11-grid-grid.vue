d3fe4d711e16cd13cbcc3b7fb4048d101aa62d8c
Yoyo San <veselu@gmail.com>
2020-08-16 14:49:44 +0300

    style(Ex 11): finish exercise

--- src/pages/11-grid-grid.vue
+++ src/pages/11-grid-grid.vue
@@ -16,7 +16,7 @@
       <q-expansion-item
         expand-separator
         v-model="expanded"
-        label="10. Mondrian you try it"
+        label="11. Grid grid"
         caption="Instructions"
       >
         <q-markdown :src="instructions">
@@ -28,7 +28,7 @@
 
     <div class="wrapper">
       <!-- row 1, 4 individual boxes -->
-      <article class="col-1-2">
+      <article class="r1c1">
         <h3>Boston Cream Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/boston-cream.jpg"
@@ -37,7 +37,7 @@
         <p>Boston's famous dessert, a combination of yellow cake, vanilla pudding, and chocolate frosting.</p>
         <p><a href="http://www.foodnetwork.com/recipes/boston-cream-pie-recipe0-1908702">Read more &gt;&gt;</a></p>
       </article>
-      <article class="col-1-2">
+      <article class="r1c2">
         <h3>Coconut Cream Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/coconut-cream.jpg"
@@ -46,7 +46,7 @@
         <p>Coconut cream pie will take you to an island escape. Especially delicious in winter.</p>
         <p><a href="https://www.favfamilyrecipes.com/coconut-cream-pie-2/">Read more &gt;&gt;</a></p>
       </article>
-      <article class="col-1-2">
+      <article class="r1c3">
         <h3>Key Lime Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/keylime-pie.jpg"
@@ -58,7 +58,7 @@
             target="_blank"
           >Read more &gt;&gt;</a></p>
       </article>
-      <article class="col-1-2">
+      <article class="r1c4">
         <h3>Pumpkin Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/pumpkin-pie.jpg"
@@ -71,7 +71,7 @@
           >Read more &gt;&gt;</a></p>
       </article>
       <!-- row 2, 2 boxes spanning 2 rows -->
-      <article class="col-2">
+      <article class="r2c1">
         <h3>Key Lime Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/keylime-pie.jpg"
@@ -83,7 +83,7 @@
             target="_blank"
           >Read more &gt;&gt;</a></p>
       </article>
-      <article class="col-2">
+      <article class="r2c2">
         <h3>Pumpkin Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/pumpkin-pie.jpg"
@@ -96,7 +96,7 @@
           >Read more &gt;&gt;</a></p>
       </article>
       <!-- row 3: narrow left column, wide right/content column -->
-      <article class="col-3 order-second">
+      <article class="r3c1">
         <h3>Coconut Cream Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/coconut-cream.jpg"
@@ -105,7 +105,7 @@
         <p>Coconut cream pie will take you to an island escape. Especially delicious in winter.</p>
         <p><a href="https://www.favfamilyrecipes.com/coconut-cream-pie-2/">Read more &gt;&gt;</a></p>
       </article>
-      <article class="col-1-4 order-first">
+      <article class="r3c2">
         <h3>Boston Cream Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/boston-cream.jpg"
@@ -115,7 +115,7 @@
         <p><a href="http://www.foodnetwork.com/recipes/boston-cream-pie-recipe0-1908702">Read more &gt;&gt;</a></p>
       </article>
       <!-- row 4: one column -->
-      <article class="col-4">
+      <article class="r4c1">
         <h3>Boston Cream Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/boston-cream.jpg"
@@ -125,7 +125,7 @@
         <p><a href="http://www.foodnetwork.com/recipes/boston-cream-pie-recipe0-1908702">Read more &gt;&gt;</a></p>
       </article>
       <!-- row 5, 1-2-1 boxes -->
-      <article class="col-2-4 order-second">
+      <article class="r5c1">
         <h3>Coconut Cream Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/coconut-cream.jpg"
@@ -134,7 +134,7 @@
         <p>Coconut cream pie will take you to an island escape. Especially delicious in winter.</p>
         <p><a href="https://www.favfamilyrecipes.com/coconut-cream-pie-2/">Read more &gt;&gt;</a></p>
       </article>
-      <article class="col-1-2 order-first">
+      <article class="r5c2">
         <h3>Boston Cream Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/boston-cream.jpg"
@@ -143,7 +143,7 @@
         <p>Boston's famous dessert, a combination of yellow cake, vanilla pudding, and chocolate frosting.</p>
         <p><a href="http://www.foodnetwork.com/recipes/boston-cream-pie-recipe0-1908702">Read more &gt;&gt;</a></p>
       </article>
-      <article class="col-1-2 order-third">
+      <article class="r5c3">
         <h3>Key Lime Pie</h3>
         <img
           src="/statics/images/1-intro-to-floats/keylime-pie.jpg"
@@ -191,6 +191,8 @@ a {
   margin: 0 auto;
   float: none;
   background-color: #fff;
+  display: grid;
+  gap: 1%;
 }
 /* hack to make images flexible in our prototype */
 div img {
@@ -199,12 +201,128 @@ div img {
 }
 
 /* grid system */
+[class*="r1"] {
+  background-color: #ffc;
+}
+[class*="r2"] {
+  background-color: #ccf;
+}
+[class*="r3"] {
+  background-color: #fcf;
+}
+[class*="r4"] {
+  background-color: #cff;
+}
+[class*="r5"] {
+  background-color: #fcc;
+}
 
 /* Mobile Landscape Screen Sizes */
 @media only screen and (min-width: 480px) {
+  .wrapper {
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(5, auto);
+  }
+
+  .r1c1 {
+    grid-column: 1/3;
+    grid-row: 1/2;
+  }
+  .r1c2 {
+    grid-column: 3/5;
+    grid-row: 1/2;
+  }
+  .r1c3 {
+    grid-column: 1/3;
+    grid-row: 2/3;
+  }
+  .r1c4 {
+    grid-column: 3/5;
+    grid-row: 2/3;
+  }
+  .r2c1 {
+    grid-column: 1/3;
+    grid-row: 3/4;
+  }
+  .r2c2 {
+    grid-column: 3/5;
+    grid-row: 3/4;
+  }
+  .r3c1 {
+    grid-column: 1/5;
+    grid-row: 4/5;
+  }
+  .r3c2 {
+    grid-column: 1/5;
+    grid-row: 5/6;
+  }
+  .r4c1 {
+    grid-column: 1/5;
+    grid-row: 6/7;
+  }
+  .r5c1 {
+    grid-column: 1/3;
+    grid-row: 8/9;
+  }
+  .r5c2 {
+    grid-column: 1/5;
+    grid-row: 7/8;
+  }
+  .r5c3 {
+    grid-column: 3/5;
+    grid-row: 8/9;
+  }
 }
 
 /* Desktop screen Sizes */
 @media only screen and (min-width: 768px) {
+  .r1c1 {
+    grid-column: 1/2;
+    grid-row: 1/2;
+  }
+  .r1c2 {
+    grid-column: 2/3;
+    grid-row: 1/2;
+  }
+  .r1c3 {
+    grid-column: 3/4;
+    grid-row: 1/2;
+  }
+  .r1c4 {
+    grid-column: 4/5;
+    grid-row: 1/2;
+  }
+  .r2c1 {
+    grid-column: 1/3;
+    grid-row: 2/3;
+  }
+  .r2c2 {
+    grid-column: 3/5;
+    grid-row: 2/3;
+  }
+  .r3c1 {
+    grid-column: 1/2;
+    grid-row: 3/4;
+  }
+  .r3c2 {
+    grid-column: 2/5;
+    grid-row: 3/4;
+  }
+  .r4c1 {
+    grid-column: 1/5;
+    grid-row: 4/5;
+  }
+  .r5c1 {
+    grid-column: 1/2;
+    grid-row: 5/6;
+  }
+  .r5c2 {
+    grid-column: 2/4;
+    grid-row: 5/6;
+  }
+  .r5c3 {
+    grid-column: 4/5;
+    grid-row: 5/6;
+  }
 }
 </style>
